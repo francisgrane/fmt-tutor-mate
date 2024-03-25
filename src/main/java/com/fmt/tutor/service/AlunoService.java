68fc70aa0b3a82c16d3e85c31d0f0ff21493f237
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-25 01:34:36 -0300

    feat: create AlunoService class

---
+++ src/main/java/com/fmt/tutor/service/AlunoService.java
@@ -0,0 +1,36 @@
+package com.fmt.tutor.service;
+
+import com.fmt.tutor.model.AlunoModel;
+import com.fmt.tutor.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarAluno(AlunoModel aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    public Optional<AlunoModel> buscarAlunoPorId(Integer id) {
+        return alunoRepository.findById(id);
+    }
+
+    public ArrayList<AlunoModel> buscaTodosOsAlunos() {
+        return (ArrayList<AlunoModel>) alunoRepository.findAll();
+    }
+
+    public AlunoModel atualizarAluno(AlunoModel aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    public void deletarAlunoPorId(Integer id) {
+        alunoRepository.deleteById(id);
+    }
+}
